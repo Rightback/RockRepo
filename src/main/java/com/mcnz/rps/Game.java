08205e3dbfc42214926cf332c27a18c411b863bb
AGK-2 <mike2agk@gmail.com>
2020-06-03 23:23:01 -0400

    added 2nd line

--- src/main/java/com/mcnz/rps/Game.java
+++ src/main/java/com/mcnz/rps/Game.java
@@ -7,6 +7,7 @@ public class Game {
 	public static void main(String[] args) {
 		String prompt = "Will it be rock, paper or scissors?";
 		String input = JOptionPane.showInputDialog(prompt);
+# adding second line for cherry-picking
 		System.out.println("You selected: " + input);
 	}
 
